3dccb88a86fb1a59124d10dbd7bbe0a8182b1609
Ayden <70488944+galaxine-senpai@users.noreply.github.com>
2022-03-22 17:31:01 -0700

    Lorem Ipsum (yes placeholder text cuz I don't want to do more commit msgs)

--- cogs/owner.py
+++ cogs/owner.py
@@ -287,13 +287,13 @@ class Owner(commands.Cog):
 @commands.check(permissions.is_owner)
 async def shutdown(ctx):
     """Restarts the bot"""
-        embed = discord.Embed(color = discord.Color.from_rgb(255,255,255))
-        embed.add_field(name="Shutdown Command Sent, Bot Rebooting in 3 seconds", value = str, inline = False)
-        await ctx.send(embed=embed)
-        await asyncio.sleep(3)
-        await bot.close()
-        os.execl(sys.executable, sys.executable, * sys.argv)
-        os.system("git pull | python3 ~/Documents/microwave/index.py")
+    embed = discord.Embed(color = discord.Color.from_rgb(255,255,255))
+    embed.add_field(name="Shutdown Command Sent, Bot Rebooting in 3 seconds", value = str, inline = False)
+    await ctx.send(embed=embed)
+    await asyncio.sleep(3)
+    await Client.close()
+    os.execl(sys.executable, sys.executable, * sys.argv)
+    os.system("git pull | python3 ~/Documents/microwave/index.py")
             
 def setup(bot):
     bot.add_cog(Owner(bot))
