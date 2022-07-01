eff5e27eb2995c1327c2a73d4568d0c6a5693163
Ayden <ayden@microwavebot.tech>
2022-07-01 15:30:31 -0700

    Shop update part 1

---
+++ utils/shop.py
@@ -0,0 +1,14 @@
+shop = {
+    "items": {
+        "test": {
+            "avaliable": True,
+            "price": 100,
+            "description": "This is a test item"
+        },
+        "test-free": {
+            "avaliable": True,
+            "price": 0,
+            "description": "This is a test item" 
+        }, #TODO: test shop commands and make sure they work
+    }
+}
